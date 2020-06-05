6ccec4803f4eb2389d43261b346e200abe855af5
Tona <carlostonatihu@gmail.com>
2020-06-05 00:03:04 -0500

    Mas jodidas pruebas

--- TT/capitulo6/unitarias/codigo/test_util.py
+++ TT/capitulo6/unitarias/codigo/test_util.py
@@ -1,28 +1,7 @@
 from django.test import TestCase
-from general.util import change_time_zone, Message, ImageUtil, Validator, Email
-from django.core.exceptions import ObjectDoesNotExist
-from django.utils import timezone
+from general.util import Validator
 from users.models import User
 from proyectos.models import Proyecto
-
-class ChangeTimeZoneTest(TestCase):
-    def test_change_time_zone(self):
-        data = dict()
-        data["fechaCreacion"] = timezone.now()
-        data["fechaModificacion"] = timezone.now()
-        self.assertTrue(change_time_zone(data) is not None)
-
-class MessageTest(TestCase):
-    def test_add_alert(self):
-        message = Message()
-        message.add_alert(text = "Un mensaje")
-        self.assertEquals(message.get_len(), 1)
-
-class ImageUtilTest(TestCase):
-    def test_init(self):
-        image_util = ImageUtil()
-        self.assertEquals(image_util.build_url(1, 1, "imagen"), 
-            "http://192.168.1.68:8000/media/proyectos/1/1/imagen")
 
 class ValidatorTest(TestCase):
     def setUp(self):
@@ -71,25 +50,3 @@ class ValidatorTest(TestCase):
 
     def test_proyect_exists_false(self):
         self.assertFalse(self.validator.proyect_exists(self.user, "Proyecto 2"))
-
-
-class EmailTest(TestCase):
-    def setUp(self):
-        self.email_test = Email()
-
-    def tearDown(self):
-        self.email_test.close()
-
-    def test_close(self):
-        email = Email()
-        email.close()
-
-    def test_send_validation_email(self):
-        self.email_test.send_validation_email("calorstonatihu@gmail.com", "tona", 
-            "localhost:8080/login")
-        self.assertTrue(True)
-
-    def test_send_change_password_email(self):
-        self.email_test.send_change_password_email("calorstonatihu@gmail.com", "tona", 
-            "localhost:8080/login")
-        self.assertTrue(True)
